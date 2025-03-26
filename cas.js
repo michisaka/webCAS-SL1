5a5563a9146a8aa63a61adfdc668e9a07799c37e
Michisaka.Koshi <20223519+michisaka@users.noreply.github.com>
2025-03-26 19:47:12 +0900

    feat: 遷移規則ファイルの読み込み

--- cas.js
+++ cas.js
@@ -1,9 +1,184 @@
-export { states };
+export { states, rules, loadRuleFile };
 
-const states = [
-  { name: "www", type: "external", bgColor: "FFFFFF", fgColor: "000000" },
-  { name: "P0", type: "general", bgColor: "FFFF00", fgColor: "000000" },
-  { name: "Q", type: "soldire", bgColor: "FFFFFF", fgColor: "000000" },
-  { name: "A000", type: "other", bgColor: "FF5555", fgColor: "000000" },
-  { name: "T", type: "firing", bgColor: "FF9900", fgColor: "000000" },
+let states = [
+  { name: "G", type: "general", bgColor: "FFFF00", fgColor: "000000" },
+  { name: "Q", type: "soldier", bgColor: "FFFFFF", fgColor: "000000" },
+  { name: "A", type: "others", bgColor: "FF5555", fgColor: "000000" },
+  { name: "F", type: "firing", bgColor: "FF9900", fgColor: "000000" },
+  { name: "w", type: "external", bgColor: "808080", fgColor: "000000" },
 ];
+
+let rules = [
+  { left: "w", current: "G", right: "Q", next: "G", count: 0, totalCount: 0 },
+  { left: "G", current: "Q", right: "Q", next: "Q", count: 0, totalCount: 0 },
+  { left: "Q", current: "Q", right: "Q", next: "Q", count: 0, totalCount: 0 },
+  { left: "Q", current: "Q", right: "w", next: "Q", count: 0, totalCount: 0 },
+];
+
+function loadRuleFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", (event) => {
+      const lines = event.target.result.split(/[\r\n]+/);
+
+      try {
+        resolve(readLines(lines));
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    reader.readAsText(file);
+  });
+}
+
+function readLines(lines) {
+  const STATE_PATTERN = /^state_number\s+\d+\s*$/;
+  const GENERAL_PATTERN = /^general_number\s+\d+\s*$/;
+  const RULE_PATTERN = /^rule_number\s+\d+\s*$/;
+
+  const stateNames = new Set();
+  const usedStateTypes = new Set();
+
+  const definedRules = new Set();
+
+  const tempStates = [];
+  const tempRules = [];
+
+  let parser;
+
+  if (lines[0] !== "SL1rule") {
+    reject(
+      new Error(`ルール種別を識別できませんでした。 1行目: "${lines[0]}""`),
+    );
+  }
+
+  let lineNum;
+  for (lineNum = 1; lineNum < lines.length; lineNum++) {
+    if (/^\s*$/.test(lines[lineNum])) {
+      continue;
+    }
+
+    if (STATE_PATTERN.test(lines[lineNum])) {
+      parser = stateParser;
+      continue;
+    }
+    if (GENERAL_PATTERN.test(lines[lineNum])) {
+      parser = generalParser;
+      continue;
+    }
+    if (RULE_PATTERN.test(lines[lineNum])) {
+      parser = ruleParser;
+      continue;
+    }
+
+    parser(lines[lineNum]);
+  }
+
+  const mustStateTypes = new Set([
+    "external",
+    "general",
+    "soldier",
+    "others",
+    "firing",
+  ]);
+
+  const undefinedStateTypes = mustStateTypes.difference(usedStateTypes);
+  if (undefinedStateTypes.size !== 0) {
+    throw new Error(
+      `状態"${undefinedStateTypes.values().toArray().join(",")}"が定義されていません。"`,
+    );
+  }
+  states = tempStates;
+  rules = tempRules;
+
+  return;
+
+  function stateParser(line) {
+    const STATE_LINE_PATTERN =
+      /^(?<name>\S{1,5})\s*@(?<fg>[0-9A-F]{6}),(?<bg>[0-9A-F]{6}),(?<type>\S+)$/;
+
+    const result = line.match(STATE_LINE_PATTERN);
+    if (!result) {
+      throw new Error(
+        `状態の読み込みに失敗しました。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+      );
+    }
+
+    const bg = result.groups.bg;
+    const fg = result.groups.fg;
+    const state = {
+      name: result.groups.name,
+      type: result.groups.type,
+      bgColor: [bg[4], bg[5], bg[2], bg[3], bg[0], bg[1]].join(""),
+      fgColor: [fg[4], fg[5], fg[2], fg[3], fg[0], fg[1]].join(""),
+    };
+
+    if (stateNames.has(state.name)) {
+      throw new Error(
+        `状態名"${state.name}"はすでに定義されています。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+      );
+    } else {
+      stateNames.add(state.name);
+    }
+
+    if (state.type !== "others" && usedStateTypes.has(state.type)) {
+      throw new Error(
+        `状態"${state.type}"はすでに定義されています。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+      );
+    } else {
+      usedStateTypes.add(state.type);
+    }
+
+    tempStates.push(state);
+  }
+
+  function generalParser(_) {
+    /* do notiong. */
+  }
+
+  function ruleParser(line) {
+    const RULE_LINE_PATTERN =
+      /^(?<left>\S{1,5})\s*##(?<current>\S{1,5})\s*##(?<right>\S{1,5})\s*->(?<next>\S{1,5})\s*$/;
+
+    const result = line.match(RULE_LINE_PATTERN);
+    if (!result) {
+      throw new Error(
+        `遷移規則の読み込みに失敗しました。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+      );
+    }
+
+    const left = result.groups.left;
+    const current = result.groups.current;
+    const right = result.groups.right;
+    const next = result.groups.next;
+
+    [left, current, right, next].forEach((name) => {
+      if (!stateNames.has(name)) {
+        throw new Error(
+          `未定義の状態名"${name}"が使用されました。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+        );
+      }
+    });
+
+    const currentState = `${left}\0${current}\0${right}`;
+
+    if (definedRules.has(currentState)) {
+      throw new Error(
+        `同じ近傍状態の遷移規則がすでに定義されています。 ${lineNum + 1}行目: "${lines[lineNum]}"`,
+      );
+    } else {
+      definedRules.add(currentState);
+    }
+
+    tempRules.push({
+      left: left,
+      current: current,
+      right: right,
+      next: next,
+      count: 0,
+      totalCount: 0,
+    });
+  }
+}
--- web-cas.js
+++ web-cas.js
@@ -1,7 +1,24 @@
-import { states } from "./cas.js";
+import { states, rules, loadRuleFile } from "./cas.js";
 
 let cellSize = 10;
 const cellTransition = document.querySelector("tbody#cell-transition");
+
+const input = document.querySelector("input#rule-file");
+input.addEventListener("change", async function (event) {
+  if (event.target.files.length === 0) {
+    return;
+  }
+  try {
+    await loadRuleFile(event.target.files[0]);
+    setupStyleSheet(states);
+    initializeStateTable();
+    initializeRuleTable();
+    initializeCellArray(cellSize);
+  } catch (error) {
+    alert(`遷移規則ファイルの読み込みに失敗しました。\n${error.message}`);
+    event.target.value = null;
+  }
+});
 
 function setupStyleSheet(states) {
   while (document.styleSheets[1].cssRules.length > 0) {
@@ -9,9 +26,94 @@ function setupStyleSheet(states) {
   }
 
   for (const state of states) {
-    let newStyle = `table.cell-table td[data-state="${state.name}"] { color: #${state.fgColor}; background-color: #${state.bgColor}; }`;
+    let newStyle = `td[data-state="${state.name}"] { color: #${state.fgColor}; background-color: #${state.bgColor}; }`;
     document.styleSheets[1].insertRule(newStyle);
   }
+}
+
+function initializeStateTable() {
+  const stateRows = document.querySelector("tbody#state-rows");
+  stateRows.innerHTML = null;
+
+  let stateCount = 0;
+  for (const state of states) {
+    const stateRow = document.createElement("tr");
+
+    const stateName = document.createElement("td");
+    stateName.dataset.state = state.name;
+    stateName.textContent = state.name;
+    stateName.classList.add("state-name");
+    stateRow.append(stateName);
+
+    const stateType = document.createElement("td");
+    stateType.textContent = state.type;
+    stateRow.append(stateType);
+
+    if (state.type !== "external") {
+      stateCount++;
+    }
+
+    stateRows.append(stateRow);
+  }
+  const totalStateText = document.querySelector("span#total-state-num");
+  totalStateText.textContent = stateCount;
+}
+
+function initializeRuleTable() {
+  const ruleRows = document.querySelector("tbody#rule-rows");
+  ruleRows.innerHTML = null;
+
+  rules.forEach((rule, i) => {
+    const ruleRow = document.createElement("tr");
+    ruleRow.dataset.rulenum = i;
+
+    const ruleNum = document.createElement("th");
+    ruleNum.textContent = i + 1;
+    ruleRow.append(ruleNum);
+
+    const leftState = document.createElement("td");
+    leftState.dataset.state = rule.left;
+    leftState.textContent = rule.left;
+    leftState.classList.add("state-name");
+    ruleRow.append(leftState);
+
+    const currentState = document.createElement("td");
+    currentState.dataset.state = rule.current;
+    currentState.textContent = rule.current;
+    currentState.classList.add("state-name");
+    ruleRow.append(currentState);
+
+    const rightState = document.createElement("td");
+    rightState.dataset.state = rule.right;
+    rightState.textContent = rule.right;
+    rightState.classList.add("state-name");
+    ruleRow.append(rightState);
+
+    const gap = document.createElement("td");
+    gap.textContent = "=>";
+    ruleRow.append(gap);
+
+    const nextState = document.createElement("td");
+    nextState.dataset.state = rule.next;
+    nextState.textContent = rule.next;
+    nextState.classList.add("state-name");
+    ruleRow.append(nextState);
+
+    // const count = document.createElement("td");
+    // count.textContent = rule.count;
+    // count.classList.add("ref-count");
+    // ruleRow.append(count);
+
+    // const totalCount = document.createElement("td");
+    // totalCount.textContent = rule.totalCount;
+    // totalCount.classList.add("ref-count");
+    // ruleRow.append(totalCount);
+
+    ruleRows.append(ruleRow);
+  });
+
+  const totalRuleText = document.querySelector("span#total-rule-num");
+  totalRuleText.textContent = rules.length;
 }
 
 function initializeCellArray(cellSize, generalPos = 0) {
@@ -19,7 +121,7 @@ function initializeCellArray(cellSize, generalPos = 0) {
 
   const external = states.find((state) => state.type === "external");
   const general = states.find((state) => state.type === "general");
-  const soldire = states.find((state) => state.type === "soldire");
+  const soldier = states.find((state) => state.type === "soldier");
 
   const externalTh = document.createElement("th");
   externalTh.classList.add("external");
@@ -30,10 +132,12 @@ function initializeCellArray(cellSize, generalPos = 0) {
   cellTransition.innerHTML = null;
   cellArrayHeader.innerHTML = null;
   const firstTr = document.createElement("tr");
+  firstTr.dataset.step = 0;
 
   cellArrayHeader.append(document.createElement("th"));
   const stepZeroTh = document.createElement("th");
   stepZeroTh.textContent = "0";
+  stepZeroTh.classList.add("step-num");
   firstTr.append(stepZeroTh);
 
   cellArrayHeader.append(externalTh.cloneNode());
@@ -49,9 +153,10 @@ function initializeCellArray(cellSize, generalPos = 0) {
       td.dataset.state = general.name;
       td.textContent = general.name;
     } else {
-      td.dataset.state = soldire.name;
-      td.textContent = soldire.name;
+      td.dataset.state = soldier.name;
+      td.textContent = soldier.name;
     }
+    td.dataset.pos = `${i + 1},0`;
     firstTr.append(td);
   }
 
@@ -62,5 +167,7 @@ function initializeCellArray(cellSize, generalPos = 0) {
 
 document.addEventListener("DOMContentLoaded", () => {
   setupStyleSheet(states);
+  initializeStateTable();
+  initializeRuleTable();
   initializeCellArray(cellSize);
 });
